2ff61942e8a4399328cdfdc1a8a825313a2036d5
PallaDeepika <palladeepika245@gmail.com>
2021-11-08 17:07:44 +0530

    set state based on prev state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,29 +1,47 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
-class Message extends Component {
-    constructor() {
-        super()
+class Counter extends Component {
+
+    constructor(props) {
+        super(props)
+
         this.state = {
-            message: 'welcome visitor to dxc'  //message partition
+            count: 0
         }
+    }
+
+    increase() {
+       /*  this.setState({                        //setState method is async  --- executed on a different thread // it will re render the component
+            count: this.state.count + 1
+
+        },
+         () => { console.log('call back value ', this.state.count) })   //callback function(=>arrow function)  -- please execute this function[callback] only after the state has been set
+       
+        console.log(this.state.count)          //console is being written before setState method completes
+    */ 
+        this.setState((prevState,props) => ({
+            count: prevState.count + props.addValue
+        }))
+        console.log(this.state.count)
+    }
+
+
+    increaseFive(){
+        this.increase()
+        this.increase()
+        this.increase()
+        this.increase()
+        this.increase()
     }
 
     render() {
         return (
             <div>
-                <h1>{this.state.message}</h1>
-                <button   onClick = {()=> this.changeMessage()}>subscribe</button>
-
+                Count - {this.state.count}
+                <button onClick={() => this.increaseFive()}>increment</button>
             </div>
         )
     }
-
-    changeMessage(){
-        this.setState({    //changing the content of the partition
-            message: 'thank you for subscribing'
-        })
-    }
-
 }
 
-export default Message
+export default Counter
