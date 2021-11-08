ab2887596cd7510f6358e96a1cf0662b53b1ad4b
PallaDeepika <palladeepika245@gmail.com>
2021-11-08 16:29:46 +0530

    Counter increment button click

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
