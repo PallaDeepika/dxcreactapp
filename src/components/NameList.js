ab692eb0f3db8d09b60ac711ed16a188183d42ad
PallaDeepika <palladeepika245@gmail.com>
2021-11-09 17:13:00 +0530

    Person.js seperated person component from person list component

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,42 +1,43 @@
 import React from 'react'
+import Person from './Person'
 
 function NameList() {
-        const persons = [
-            {
-                id:1,
-                name: 'deepu',
-                age: 21,
-                skill: 'react'
-            },
-            {
-                id:1,
-                name: 'priya',
-                age: 20,
-                skill: 'flutter'
-            },
-            {
-                id:1,
-                name:'lucky',
-                age: 20,
-                skill: 'java'
-            },
-            {
-                id:1,
-                name:'uma',
-                age: 20,
-                skill: 'spring'
-            }
-        ]
-    
+    const persons = [
+        {
+            id: 1,
+            name: 'deepu',
+            age: 21,
+            skill: 'react'
+        },
+        {
+            id: 1,
+            name: 'priya',
+            age: 20,
+            skill: 'flutter'
+        },
+        {
+            id: 1,
+            name: 'lucky',
+            age: 20,
+            skill: 'java'
+        },
+        {
+            id: 1,
+            name: 'uma',
+            age: 20,
+            skill: 'spring'
+        }
+    ]
 
 
-    /* const names = ['deepu', 'priya', 'lucky', 'dxc','react']
+
+    /* const names = ['abdul', 'ansari', 'tanveer', 'dxc','react']
     const namesList = names.map(name => <h2>{name}</h2>) */
-    const personList = persons.map(person => <h2> i am {person.name},i am {person.age} years old and my skill is {person.skill}</h2>)
+    const personList = persons.map(person => (<Person human = {person}></Person>))
 
     return (
         <div>{personList}</div>
     )
- }
+}
 
 export default NameList
